fdab9767214850535268bf60d2f6e97d04de7337
Bruno Oliveira <69878902+brunopdoliveira@users.noreply.github.com>
2024-09-21 14:17:47 -0300

    Delete Virtualization/monitor.sh

--- Virtualization/monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "-h" ] || [ "$1" == '' ]; then
-  echo "Usage: `basename $0` [vm_name]"
-  echo 
-  exit 0
-fi
-echo "A tecla de escape para sair do monitor da VM ${1} eh Ctrl + q"
-socat unix:/vms-kvm/monitor/${1}.sock stdio,raw,echo=0,escape=0x11
